d3af9edaa0aa91df6f405256ab142187f37082a8
spyder-s <sandeepilmtechnosolutions@gmail.com>
2020-04-08 15:20:07 +0530

    create message module.

--- resources/views/posts/chat.blade.php
+++ resources/views/posts/chat.blade.php
@@ -13,6 +13,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
+
                     <div class="card-header">Messages</div>
                     <div class="card-body">
                         <div class="form-group ex3">
